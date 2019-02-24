8d4ef28db9332c77fe8513bed5b7f5daa07eee49
AKN <anilnarayanan@hotmail.com>
2019-02-24 06:04:37 +0530

    Author: Smita Nair Version: 0.1

---
+++ src/upgrad/BloomFilter.java
@@ -0,0 +1,77 @@
+package upgrad;
+
+import java.util.BitSet;
+
+/*** 
+ * 
+ * @author upgrad.com
+ * 
+ * The Bloomfilter logic is implemented with help of this class 
+ * And we use CustomHashFunction class to build our BloomFilter.
+ */
+public class BloomFilter {
+	BitSet bs;//BloomFilter array of bits
+	
+	CustomHashFunction chf;
+	int collisionCount = 0;
+	int hash1, hash2, hash3, hash4, hash5, hash6, hash7; //hash function values
+	int size; //size of the bloom filter
+	int numHashFunctions; //total number of hash functions to work with bloom filter
+	
+	public BloomFilter(int x, int y) {
+		/*** Constructor
+		 * x indicate the size of bloom filter and y indicate the number of hash functions
+		 */
+		bs = new BitSet(x);
+		bs.clear();
+		chf = new CustomHashFunction();
+		size = bs.size();
+		System.out.println("Size: " + size);
+		numHashFunctions = y;
+	}
+	
+	public boolean isPresent(String x) {
+		/***
+		 * This function returns true is x is present and false if x is not present-
+		 * according to bloom filter.
+		 */
+		hash1 = chf.genHash(x);
+		hash2 = chf.genHash2(x);
+
+		for(int i = 1; i <= numHashFunctions; i++) {
+            if (bs.get(Math.abs((hash1 + i * hash2) % size))) {
+                continue;
+            } else {
+                return false;
+            }
+        }
+		return true;
+	}
+	
+	public void add(String x) {
+		/***
+		 * Build the bloom filter by adding the elements to it
+		 */
+//		if(isPresent(x)) {
+//			collisionCount++;
+//		}
+		
+		hash1 = Math.abs(chf.genHash(x));
+		hash2 = Math.abs(chf.genHash2(x));
+//        hash3 = Math.abs(chf.genHash3(x)%10);
+//        hash4 = chf.genHash4(x);
+//        hash5 = chf.genHash5(x);
+//        hash6 = chf.genHash6(x);
+//        hash7 = chf.genHash7(x);
+//		System.out.println(hash1);
+//		System.out.println(hash2);
+//		System.out.println(hash3);
+		for(int i = 1; i <= numHashFunctions; i++) {
+			bs.set(Math.abs((hash1 + i *hash2) % size));
+//			bs.set(Math.abs((hash1) % size));
+//			bs.set(Math.abs((hash2) % size));
+//			bs.set(Math.abs((hash3) % size));
+		}
+	}
+	
+}
---
+++ src/upgrad/RunApp.java
@@ -0,0 +1,58 @@
+package upgrad;
+
+import java.io.*;
+import java.util.BitSet;
+import java.util.Scanner;
+
+public class RunApp {
+	static BufferedReader br = null;
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//List of weak passwords
+		String fileLocation1 = "/home/anushka/Downloads/BloomFilterApp/data/validVotersList.txt";
+		Scanner input = new Scanner(System.in);
+		int counter = 500000; // to use 2/3 to build bloom filter and use 1/3 to test the bloom filter.
+		int initial_counter = counter;
+		int numFalsePos = 0;
+		String l = null;
+		String voter_id = null;
+		BloomFilter bf = new BloomFilter(7298441, 2);
+
+		try {
+			br = new BufferedReader(new FileReader(fileLocation1));
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+
+		try {
+			while((l = br.readLine()) != null) {
+//				if(counter > initial_counter/3) {
+
+				bf.add(l);
+//				}
+//				else {
+//					if(bf.isPresent(l)) {
+//						numFalsePos++;
+//					}
+//				}
+//				counter--;
+			}
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		System.out.println("total number of collisions while building a bloom filter are " + bf.collisionCount);
+		System.out.println("Total number of false positives are " + numFalsePos);
+
+
+//		================================================================
+		ElectionCount ec = new ElectionCount();
+		ec.add(bf);
+		System.out.println("Enter Voter Id: ");
+		voter_id = input.nextLine();
+		ec.find(voter_id.trim());
+	}
+
+}
